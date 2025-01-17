7605b6e51ab35d34da603365f2651db9a1f82583
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-01-18 01:48:46 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const Welcome = () => {
+  return <h1>Welcome to react world developer</h1>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-
-reportWebVitals();
-
+const Website = () => {
+  return (
+    <div>
+      <Welcome />
+      <h2>
+        Fatemeh Madihi and I am <span>{Math.floor(Math.random() * 30)} years old.</span>
+      </h2>
+    </div>
+  );
+};
+export default Website;
