b8f90e2fc62c706b30b2773c641701576fba054b
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-01-18 02:09:25 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Website,{Welcome} from './component/Welcome';
 function App() {
   return (
     <div className="App">
-      <Welcome />
-      <Website />
+      <Website name="Fatemeh Madihi" age = "30" />
+      <Website name="ali ahmadi" age = "20" />
     </div>
   );
 }
